5cbee3ade2c63b25704166e606103ca044df1a73
Eduardo Dornelles <e_dornelles1992@hotmail.com>
2020-06-22 22:04:11 -0300

    ajustado rota default e adicionado componente de notificação

--- src/App.js
+++ src/App.js
@@ -2,11 +2,14 @@ import React from 'react';
 import './App.css';
 import Routes from './config/routes';
 import CssBaseline from '@material-ui/core/CssBaseline';
+import ReactNotification from 'react-notifications-component';
+import 'react-notifications-component/dist/theme.css'
 
 function App() {
   return (
     <div className="App">
       <CssBaseline />
+      <ReactNotification />
       <Routes />
     </div>
   );

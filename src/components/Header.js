ba9d3c92cbc2fac3c4503faa29685a3c80c9ef23
Eduardo Dornelles <e_dornelles1992@hotmail.com>
2020-06-25 00:16:19 -0300

    adicionado tela inicial e header

---
+++ src/components/Header.js
@@ -0,0 +1,58 @@
+import React, { Component } from 'react';
+import AppBar from '@material-ui/core/AppBar';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+import { Link } from "react-router-dom";
+
+export default class Header extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            value: -1,
+            header: null,
+        };
+    }
+
+    setHeaderAdmin() {
+        return (
+            <AppBar position="static">
+                <Tabs aria-label="simple tabs example">
+                    <Tab component={Link} to={'/alunos'} label="Alunos" />
+                    <Tab component={Link} to={'/times'} label="Times" />
+                    <Tab component={Link} to={'/resultados'} label="Resultados" />
+                </Tabs>
+            </AppBar>
+        )
+    }
+
+    setHeaderAvaliador() {
+        return (
+            <AppBar position="static">
+                <Tabs aria-label="simple tabs example">
+                    <Tab component={Link} to={'/avaliacao'} label="Avaliação" />
+                    <Tab component={Link} to={'/resultados'} label="Resultados" />
+                </Tabs>
+            </AppBar>
+        )
+    }
+
+
+    async componentDidUpdate() {
+        console.log("entrou")
+        const value = await sessionStorage.getItem("tipoUsuario")
+        if (this.state.value != value)
+         this.setState({
+                value: value,
+                header: value == 1 ? this.setHeaderAdmin() : this.setHeaderAvaliador()
+         }) 
+    }
+
+    render() {
+        return (
+            <>
+        {this.state.header}
+        {this.props.children}
+        </>
+        )
+    }
+}
--- src/config/routes.js
+++ src/config/routes.js
@@ -1,18 +1,16 @@
 import React from 'react';
 import {BrowserRouter as Router} from 'react-router-dom';
-import { Link } from "react-router-dom";
 import { Route, Switch, Redirect } from "react-router";
 import { createBrowserHistory } from "history";
+import Home from '../views/Home';
 import ListaAlunos from '../views/ListaAlunos';
 import ListaTimes from '../views/ListaTimes';
 import CadastroTime from '../views/CadastroTime'
 import ListaIntegrantes from '../views/ListaIntegrantes';
-import AppBar from '@material-ui/core/AppBar';
-import Tabs from '@material-ui/core/Tabs';
-import Tab from '@material-ui/core/Tab';
 import FichaAvaliacao from "../views/FichaAvaliacao";
 import Resultados from '../views/Resultados';
 import CadastroAlunos from '../views/CadastroAlunos';
+import Header from '../components/Header'
 
 export default function Routes() {
 
@@ -20,18 +18,10 @@ export default function Routes() {
     
     return (
         <Router history={history}>
-            <AppBar position="static">
-                <Tabs aria-label="simple tabs example">
-                    <Tab component={Link} to={'/alunos'} label="Alunos" />
-                    <Tab component={Link} to={'/times'} label="Times" />
-                    <Tab component={Link} to={'/avaliacao'} label="Avaliação" />
-                    <Tab component={Link} to={'/resultados'} label="Resultados" />
-                </Tabs>
-            </AppBar>
+              
             <Switch>
-            <Route exact path="/">
-                 <Redirect to="/alunos" />
-            </Route>
+            <Header>
+              <Route exact path="/" component={Home} />
                 <Route exact path="/alunos" component={ListaAlunos}/>
                 <Route exact path="/alunos/cadastro" component={CadastroAlunos}/>
                 <Route exact path="/times" component={ListaTimes}/>
@@ -39,7 +29,8 @@ export default function Routes() {
                 <Route exact path="/integrantes" component={ListaIntegrantes}/>
                 <Route exact path="/avaliacao" component={FichaAvaliacao}/>
                 <Route exact path="/resultados" component={Resultados}/>
-            </Switch>
+                </Header>
+            </Switch>        
         </Router>
     );
 }
